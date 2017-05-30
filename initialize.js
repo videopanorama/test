dd5f6377f0c3458801aa6db1f569c6380f5d0d27
videopanorama <videopanorama123@gmail.com>
2017-05-30 11:49:43 +0800

    .
    .

---
+++ initialize.js
@@ -0,0 +1,11 @@
+template = 'test';
+
+var ntiles = 16;
+
+$(document).ready(function() {
+    for (tile = 1; tile < ntiles; tile++) {
+    		$("#videos").append('<div class="col-lg-3 space-0"><video width="292.5" height="144" mediagroup="main" id="' + tile + '" class="video-js" preload="auto"><source src="timer2.mp4"></video></div>');
+    }
+    vidSetup();
+
+});
---
+++ vidsetup.js
@@ -0,0 +1,5 @@
+function vidSetup() {
+    for (i = 0; i < ntiles; i++) {
+        videojs(i.toString());
+   }
+}
