058aaa17c41c573c151a2644434ae85fe6755ac5
videopanorama <videopanorama123@gmail.com>
2017-05-30 13:21:41 +0800

    .
    .

--- initialize.js
+++ initialize.js
@@ -4,7 +4,7 @@ var ntiles = 16;
 
 $(document).ready(function() {
     for (tile = 1; tile < ntiles; tile++) {
-    		$("#videos").append('<div class="col-lg-3 space-0"><video width="292.5" height="144" mediagroup="main" id="' + tile + '" class="video-js" preload="auto"><source src="timer2.mp4"></video></div>');
+    		$("#videos").append('<div class="col-sm-3 space-0"><video width="292.5" height="144" mediagroup="main" id="' + tile + '" class="video-js" preload="auto"><source src="timer2.mp4"></video></div>');
     }
     vidSetup();
 
