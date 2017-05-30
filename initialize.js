1d0ee3ab2c3f21e971977092f9f2f0cea051d9ab
videopanorama <videopanorama123@gmail.com>
2017-05-30 16:15:02 +0800

    .
    .

--- initialize.js
+++ initialize.js
@@ -4,7 +4,7 @@ var ntiles = 16;
 
 $(document).ready(function() {
     for (tile = 0; tile < ntiles; tile++) {
-    		$("#videos").append('<div class="col-sm-3 space-0"><video height="170" width="auto" mediagroup="main" id="' + tile + '" class="video-js" preload="auto"><source src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"></video></div>');
+    		$("#videos").append('<div class="col-sm-3 space-0"><video height="170" width="auto" mediagroup="main" id="' + tile + '" class="video-js"><source src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"></video></div>');
     }
     vidSetup();
 
--- vidsetup.js
+++ vidsetup.js
@@ -1,3 +1,5 @@
+
+
 function vidSetup() {
     for (i = 0; i < ntiles; i++) {
         videojs(i.toString(), {loop: true, loadingSpinner: false});
